a13731d615f08953d3dfaef51151ebf8479ca035
ZannaJ <zhanna.jelatanceva@gmail.com>
2021-08-09 21:55:29 +0300

    Initial commit

---
+++ src/main/java/com/company/dbhelper/DbConnection.java
@@ -0,0 +1,22 @@
+package com.company.dbhelper;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnection {
+    public static Connection getConnection() {
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/hospital", User.USERNAME.getValue(), User.PASSWORD.getValue());
+
+
+        } catch (SQLException e) {
+//            System.out.println("Unable to connect to database");
+            e.printStackTrace();
+
+        }
+        return connection;
+    }
+}
